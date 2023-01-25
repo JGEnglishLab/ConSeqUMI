dcd06d24adff786e641114ee431a59ad69defdae
CCranney <cwcranney@proton.me>
2023-01-25 12:16:56 -0700

    finding pairwise difference function initialized. About to overhaul it

--- src/consensus/ConsensusStrategyPairwise.py
+++ src/consensus/ConsensusStrategyPairwise.py
@@ -17,5 +17,43 @@ class ConsensusStrategyPairwise(ConsensusStrategy):
             alignedScores.append(alignments[0].score)
         return mean(alignedScores)
 
+    def find_all_differences_between_two_sequences(self, originalSequence, differentSequence):
+        alignments = self.aligner.align(originalSequence, differentSequence)
+        matchingIndices = alignments[0].aligned
+
+        originalSequenceMatchingIndices, differentSequenceMatchingIndices = list(matchingIndices[0]), list(matchingIndices[1])
+
+        startingIndices = (0,0)
+        originalSequenceMatchingIndices.insert(0,startingIndices)
+        differentSequenceMatchingIndices.insert(0,startingIndices)
+
+        originalSequenceEndingIndices = (len(originalSequence),len(originalSequence))
+        differentSequenceEndingIndices = (len(differentSequence),len(differentSequence))
+        originalSequenceMatchingIndices.append(originalSequenceEndingIndices)
+        differentSequenceMatchingIndices.append(differentSequenceEndingIndices)
+
+        differencesFromOriginal = []
+        numMatchingIndices = len(originalSequenceMatchingIndices)
+        print()
+        print(alignments[0].format().split("\n")[1])
+        for i in range(numMatchingIndices - 1):
+            originalSequenceDifferenceStartIndex = originalSequenceMatchingIndices[i][1]
+            originalSequenceDifferenceEndIndex = originalSequenceMatchingIndices[i + 1][0]
+            differentSequenceInsertStartIndex = differentSequenceMatchingIndices[i][1]
+            differentSequenceInsertEndIndex = differentSequenceMatchingIndices[i + 1][0]
+            differentSequenceInsert = differentSequence[differentSequenceInsertStartIndex:differentSequenceInsertEndIndex]
+            if originalSequenceDifferenceStartIndex == originalSequenceDifferenceEndIndex and len(differentSequenceInsert) == 0: continue
+            differencesFromOriginal.append((originalSequenceDifferenceStartIndex, originalSequenceDifferenceEndIndex, differentSequenceInsert))
+
+        print(originalSequence)
+        print(differentSequence)
+        print(alignments[0])
+        print(originalSequenceMatchingIndices)
+        print(differentSequenceMatchingIndices)
+        print(differencesFromOriginal)
+
+        return differencesFromOriginal
+
+
     def generate_consensus_sequence_from_records(self, binRecords: list) -> str:
         pass
--- tests/consensus/test_ConsensusStrategyPairwise.py
+++ tests/consensus/test_ConsensusStrategyPairwise.py
@@ -1,6 +1,7 @@
 from pytestConsensusFixtures import consensusSequence, readSequences, readSequenceRecords
 import pytest
 import sys
+import random
 
 sys.path.insert(1, '/Users/calebcranney/Documents/Projects/JGEnglishLab/longread_umi_python/src')
 from consensus.ConsensusStrategyPairwise import ConsensusStrategyPairwise
@@ -33,4 +34,64 @@ def test_consensus_strategy_find_average_pairwise_alignment_score(consensusSeque
     averagePairwiseAlignmentScoreOutput = consensusStrategyPairwise.find_average_pairwise_alignment_score(consensusSequence, readSequences)
     assert averagePairwiseAlignmentScore == averagePairwiseAlignmentScoreOutput
 
+@pytest.fixture
+def originalSequence():
+    random.seed(0)
+    return "".join(random.choices("ATGC", k=20))
+
+def test_consensus_strategy_find_all_differences_between_two_sequences_finds_insertions(originalSequence, consensusStrategyPairwise):
+    insertionValue = "RRRR"
+    insertionAtFrontSequence = insertionValue + originalSequence
+    insertionAtFrontDifference = (0,0,insertionValue)
+    insertionAtFrontDifferenceOutputList = consensusStrategyPairwise.find_all_differences_between_two_sequences(originalSequence, insertionAtFrontSequence)
+    assert insertionAtFrontDifferenceOutputList[0] == insertionAtFrontDifference
+
+    insertionAtBackSequence = originalSequence + insertionValue
+    insertionAtBackDifference = (len(originalSequence),len(originalSequence),insertionValue)
+    insertionAtBackDifferenceOutputList = consensusStrategyPairwise.find_all_differences_between_two_sequences(originalSequence, insertionAtBackSequence)
+    assert insertionAtBackDifferenceOutputList[0] == insertionAtBackDifference
+
+    insertionIndex = 10
+    insertionInMiddleSequence = originalSequence[:insertionIndex] + insertionValue + originalSequence[insertionIndex:]
+    insertionInMiddleDifference = (insertionIndex,insertionIndex,insertionValue)
+    insertionInMiddleDifferenceOutputList = consensusStrategyPairwise.find_all_differences_between_two_sequences(originalSequence, insertionInMiddleSequence)
+    assert insertionInMiddleDifferenceOutputList[0] == insertionInMiddleDifference
+
+def test_consensus_strategy_find_all_differences_between_two_sequences_finds_deletions(originalSequence, consensusStrategyPairwise):
+    insertionValue = "RRRR"
+    insertionIndex = 10
+    originalSequenceEditedForDeletion = insertionValue + originalSequence[:insertionIndex] + insertionValue + originalSequence[insertionIndex + len(insertionValue):] + insertionValue
+    deletionAtFrontSequence = originalSequenceEditedForDeletion[len(insertionValue):]
+    deletionAtFrontDifference = (0,len(insertionValue),"")
+    deletionAtFrontDifferenceOutputList = consensusStrategyPairwise.find_all_differences_between_two_sequences(originalSequenceEditedForDeletion, deletionAtFrontSequence)
+    assert deletionAtFrontDifferenceOutputList[0] == deletionAtFrontDifference
+
+    deletionAtBackSequence = originalSequenceEditedForDeletion[:-len(insertionValue)]
+    deletionAtBackDifference = (len(originalSequenceEditedForDeletion)-len(insertionValue),len(originalSequenceEditedForDeletion),"")
+    deletionAtBackDifferenceOutputList = consensusStrategyPairwise.find_all_differences_between_two_sequences(originalSequenceEditedForDeletion, deletionAtBackSequence)
+    assert deletionAtBackDifferenceOutputList[0] == deletionAtBackDifference
+
+    deletionInMiddleSequence = originalSequenceEditedForDeletion[:insertionIndex] + originalSequenceEditedForDeletion[insertionIndex+len(insertionValue):]
+    deletionInMiddleDifference = (insertionIndex,insertionIndex+len(insertionValue),"")
+    deletionInMiddleDifferenceOutputList = consensusStrategyPairwise.find_all_differences_between_two_sequences(originalSequenceEditedForDeletion, deletionInMiddleSequence)
+    assert deletionInMiddleDifferenceOutputList[0] == deletionInMiddleDifference
+'''
+def test_consensus_strategy_find_all_differences_between_two_sequences_finds_mutations(originalSequence, consensusStrategyPairwise):
+    mutationValue = "RRRR"
+    mutationAtFrontSequence = mutationValue + originalSequence[len(mutationValue):]
+    mutationAtFrontDifference = (0,len(mutationValue),mutationValue)
+    mutationAtFrontDifferenceOutputList = consensusStrategyPairwise.find_all_differences_between_two_sequences(originalSequence, mutationAtFrontSequence)
+    assert mutationAtFrontDifferenceOutputList[0] == mutationAtFrontDifference
+
+    mutationAtBackSequence = originalSequence[:-len(mutationValue)] + mutationValue
+    mutationAtBackDifference = (len(originalSequence)-len(mutationValue),len(originalSequence),mutationValue)
+    mutationAtBackDifferenceOutputList = consensusStrategyPairwise.find_all_differences_between_two_sequences(originalSequence, mutationAtBackSequence)
+    assert mutationAtBackDifferenceOutputList[0] == mutationAtBackDifference
+
+    mutationIndex = 10
+    mutationInMiddleSequence = originalSequence[:mutationIndex] + mutationValue + originalSequence[mutationIndex+len(mutationValue):]
+    mutationInMiddleDifference = (mutationIndex,mutationIndex+len(mutationValue),mutationValue)
+    mutationInMiddleDifferenceOutputList = consensusStrategyPairwise.find_all_differences_between_two_sequences(originalSequence, mutationInMiddleSequence)
+    assert mutationInMiddleDifferenceOutputList[0] == mutationInMiddleDifference
+'''
 def test_consensus_strategy_generate_consensus_sequence_from_records(): pass
