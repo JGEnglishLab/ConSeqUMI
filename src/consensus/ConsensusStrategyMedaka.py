d7025c4753935b97ddba28847c13786c841499a0
CCranney <cwcranney@proton.me>
2023-01-31 10:50:59 -0700

    initialized medaka method

---
+++ src/consensus/ConsensusStrategyMedaka.py
@@ -0,0 +1,9 @@
+from consensus.ConsensusStrategy import ConsensusStrategy
+import subprocess
+from Bio import SeqIO
+from io import StringIO
+import tempfile
+
+class ConsensusStrategyMedaka(ConsensusStrategy):
+    def generate_consensus_sequence_from_biopython_records(self, binRecords: list) -> str:
+        pass
