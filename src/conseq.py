99f3f5bad0eca965170d39a605609faf2d68aac8
CCranney <cwcranney@proton.me>
2023-02-01 10:51:28 -0700

    input, output, and adapter parameters set as required for umi command

--- src/conseq.py
+++ src/conseq.py
@@ -3,10 +3,32 @@ import argparse
 def set_command_line_settings():
     parser = argparse.ArgumentParser(description="")
     commandParser = parser.add_subparsers(dest="command", help="ConSeq Functions")
-    commandParser.add_parser(
+    umiParser = commandParser.add_parser(
         "umi",
         help="Extracts UMIs and target sequences from .fastq files",
     )
+    umiParser.add_argument(
+        "-i",
+        "--input",
+        type=str,
+        required=True,
+        help="Path to folder that only contains input Nanopore read fastq files.",
+    )
+    umiParser.add_argument(
+        "-o",
+        "--output",
+        type=str,
+        required=True,
+        help="Path for folder output. Folder should not currently exist.",
+    )
+    umiParser.add_argument(
+        "-a",
+        "--adapters",
+        type=str,
+        required=True,
+        help="A text file with f, F, r, R adapters listed. Defaults to: GAGTGTGGCTCTTCGGAT, ATCTCTACGGTGGTCCTAAATAGT, AATGATACGGCGACCACCGAGATC, and CGACATCGAGGTGCCAAAC, respectively.",
+    )
+
     return parser
 
 def check_for_invalid_input(parser, args): pass
--- tests/test_conseq.py
+++ tests/test_conseq.py
@@ -3,15 +3,17 @@ import subprocess
 import argparse
 import re
 from unittest.mock import Mock
+from tempfile import TemporaryDirectory, NamedTemporaryFile
+import atexit
 
 import sys
 import os
 srcPath = os.getcwd().split("/")[:-1]
 srcPath = "/".join(srcPath) + "/src"
 sys.path.insert(1, srcPath)
-testsPath = os.getcwd().split("/")[:-1]
-testsPath = "/".join(testsPath) + "/tests"
-sys.path.insert(1, testsPath)
+#testsPath = os.getcwd().split("/")[:-1]
+#testsPath = "/".join(testsPath) + "/tests"
+#sys.path.insert(1, testsPath)
 import conseq
 
 def run_command(commandLine):
@@ -21,11 +23,33 @@ def run_command(commandLine):
 
 @pytest.fixture
 def parser():
-    parser = conseq.set_command_line_settings()
     def mock_error(message):
         raise ValueError(message)
-    parser.error = Mock(side_effect = mock_error)
+    argparse.ArgumentParser.error = Mock(side_effect = mock_error)
+    parser = conseq.set_command_line_settings()
     return parser
+
+@pytest.fixture
+def files():
+    class fileObj:
+        def __init__(self):
+            self.inputDir = TemporaryDirectory(prefix="conseq_input_test_directory_")
+            self.outputDir = TemporaryDirectory(prefix="conseq_output_test_directory_")
+            self.adapterFile = NamedTemporaryFile(prefix="conseq_adapter_test_file_", suffix=".txt")
+    return fileObj()
+
+@pytest.fixture
+def umiArgs(files):
+    umiArgs = [
+        "umi",
+        "-i",
+        files.inputDir.name,
+        "-o",
+        files.outputDir.name,
+        "-a",
+        files.adapterFile.name,
+    ]
+    return umiArgs
 
 def test__conseq__generates_original_parser(parser): pass
 
@@ -34,3 +58,23 @@ def test__conseq__fails_when_incorrect_command_provided(parser):
     errorOutput = f"argument command: invalid choice: '{nonExistentCommand}' (choose from 'umi')"
     with pytest.raises(ValueError, match=re.escape(errorOutput)):
         args = parser.parse_args([nonExistentCommand])
+
+def test__conseq__fails_when_umi_does_not_include_input_directory(parser, files, umiArgs):
+    umiArgsWithoutInput = [umiArgs[0]] + umiArgs[3:]
+    errorOutput = "the following arguments are required: -i/--input"
+    with pytest.raises(ValueError, match=re.escape(errorOutput)):
+        args = parser.parse_args(umiArgsWithoutInput)
+
+def test__conseq__fails_when_umi_does_not_include_output_directory(parser, files, umiArgs):
+    umiArgsWithoutInput = umiArgs[:3] + umiArgs[5:]
+    errorOutput = "the following arguments are required: -o/--output"
+    with pytest.raises(ValueError, match=re.escape(errorOutput)):
+        args = parser.parse_args(umiArgsWithoutInput)
+
+def test__conseq__fails_when_umi_does_not_include_adapter_file(parser, files, umiArgs):
+    umiArgsWithoutInput = umiArgs[:5]
+    errorOutput = "the following arguments are required: -a/--adapter"
+    with pytest.raises(ValueError, match=re.escape(errorOutput)):
+        args = parser.parse_args(umiArgsWithoutInput)
+
+    
