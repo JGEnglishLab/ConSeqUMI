733d2f9e654f3ee563a05989958d59cefd879cd5
CCranney <cwcranney@proton.me>
2023-02-28 19:55:26 -0700

    cleaning up old files

--- consensus_generators/config.py
+++
@@ -1,7 +0,0 @@
-starcodeCommandLine = "starcode --seq-id"
-lamassembleCommandLine = "lamassemble dependencies_download/promethion.mat --end -g60"
-medakaCommandLine = "medaka_consensus"
-
-SCOMMAND = starcodeCommandLine.split()
-LCOMMAND = lamassembleCommandLine.split()
-MCOMMAND = medakaCommandLine.split()
