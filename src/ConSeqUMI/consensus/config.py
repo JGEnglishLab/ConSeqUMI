a7687375eb7747bec130493ebedab757c9aa169b
CCranney <cwcranney@proton.me>
2023-03-15 14:15:52 -0600

    Changing the lamassemble last-train path to a generic filler that must be replaced

--- src/ConSeqUMI/consensus/config.py
+++ src/ConSeqUMI/consensus/config.py
@@ -1,2 +1,2 @@
-lamassembleCommandLine = "lamassemble /Users/calebcranney/Documents/Projects/JGEnglishLab/longread_umi_python/dependencies_download/promethion.mat --end -g60 -m 40"
+lamassembleCommandLine = "lamassemble [PATH TO LAST-TRAIN FILE] --end -g60 -m 40"
 LCOMMAND = lamassembleCommandLine.split()
