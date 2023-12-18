6aa5bf390f1a1a3a5dee4c7a0fd6c0e2249745be
samhime92 <64994860+samhimes92@users.noreply.github.com>
2023-12-18 13:11:14 -0700

    Added option to change path to last train mat.

--- src/ConSeqUMI/consensus/config.py
+++ src/ConSeqUMI/consensus/config.py
@@ -1,5 +1,10 @@
-lamassembleCommandLine = "lamassemble [PATH TO LAST-TRAIN FILE] --end -g60 -m 40"
-LCOMMAND = lamassembleCommandLine.split()
+from typing import List
+
+LAST_TRAIN_PATH = {"ltp":"[PATH TO LAST-TRAIN FILE]"}
+lamassembleCommandLine = f"lamassemble mat_path_filled_in_programmatically --end -g60 -m 40"
+
+LCOMMAND: List[str] = lamassembleCommandLine.split()
 
 medakaCommandLine = "medaka_consensus -f -m r941_min_high_g303"
 MCOMMAND = medakaCommandLine.split()
+
